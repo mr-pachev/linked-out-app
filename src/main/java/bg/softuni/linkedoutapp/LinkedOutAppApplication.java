729c7ddddf2f6c67e51a0f5292790904ae022236
Ivan Pachev <mr.pachev@abv.bg>
2024-06-02 17:29:17 +0300

    Initial commit

---
+++ src/main/java/bg/softuni/linkedoutapp/LinkedOutAppApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.linkedoutapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedOutAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedOutAppApplication.class, args);
+	}
+
+}
