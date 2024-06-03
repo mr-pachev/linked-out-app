8e42f605b17b145c05a9e4570070323cc5cc1502
Ivan Pachev <mr.pachev@abv.bg>
2024-06-03 15:23:45 +0300

    Model Mapper convert LocalDate

--- src/main/java/bg/softuni/linkedoutapp/config/ApplicationBeanConfiguration.java
+++ src/main/java/bg/softuni/linkedoutapp/config/ApplicationBeanConfiguration.java
@@ -1,17 +1,31 @@
 package bg.softuni.linkedoutapp.config;
 
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Configuration
 public class ApplicationBeanConfiguration {
     @Bean
-    public ModelMapper modelMapper(){
-        ModelMapper mapper = new ModelMapper ();
-        return mapper;
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.addConverter(new Converter<String, LocalDate>() {
+            @Override
+            public LocalDate convert(MappingContext<String, LocalDate> mappingContext) {
+                return LocalDate
+                        .parse(mappingContext.getSource(),
+                                DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+            }
+        });
+        return modelMapper;
     }
 
     @Bean
